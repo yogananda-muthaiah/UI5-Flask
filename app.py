f21a7596284f3a27d2637e8ec5c6ca9fb00e2aa6
Yogananda Muthaiah <yogananda.muthaiah@gmail.com>
2024-11-17 13:08:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import requests
-import os
+
 
 app = Flask(__name__)
 port = int(os.getenv("PORT", 0))
 
 @app.route('/')
 def index():
+    ### Fine ##
     return render_template('index.html')
 
 @app.route('/generate_api_results', methods=['POST'])
 def generate_api_results():
+    ### Fine ##
     # Get values from the request
     input1 = request.form.get('input1')
     input2 = request.form.get('input2')
@@ -27,7 +30,6 @@ def generate_api_results():
         api_results = response.json()
 
         return jsonify(api_results)
-    
     except requests.exceptions.RequestException as e:
         # Handle request errors
         return jsonify({"error": str(e)}), 500
@@ -37,4 +39,4 @@ if __name__ == '__main__':
     if port != 0:
         app.run(host='0.0.0.0', port=port)
     else:
-        app.run()  
+        app.run()
